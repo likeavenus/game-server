538bd02555abb56eb0540429e878a835a66b0c70
Rafael <rafael_guliev94@list.ru>
2023-10-29 17:31:48 +0300

    add socket events

--- app.js
+++ app.js
@@ -20,12 +20,20 @@ const players = {};
 io.on("connection", (socket) => {
   console.log("a user connected: ", socket.id);
   players[socket.id] = {
-    x: Math.floor(Math.random() * 100) + 50,
-    y: Math.floor(Math.random() * 100) + 50,
+    x: Math.floor(Math.random() * 800),
+    y: Math.floor(Math.random() * 600),
     id: socket.id,
   };
 
   socket.broadcast.emit("new_player", players[socket.id]);
+
+  socket.on("move", (mouseCoords) => {
+    if (mouseCoords.id) {
+      players[mouseCoords.id].x = mouseCoords.x;
+      players[mouseCoords.id].y = mouseCoords.y;
+      socket.broadcast.emit("move", players[mouseCoords.id]);
+    }
+  });
 
   socket.on("disconnect", () => {
     console.log("user disconnected");
@@ -33,14 +41,6 @@ io.on("connection", (socket) => {
     delete players[socket.id];
 
     io.emit("remove_player", socket.id);
-  });
-
-  socket.on("move", (mouseCoords) => {
-    if (mouseCoords.id) {
-      players[mouseCoords.id].x = mouseCoords.x;
-      players[mouseCoords.id].y = mouseCoords.x;
-      socket.broadcast.emit("move", players[mouseCoords.id]);
-    }
   });
 });
 
