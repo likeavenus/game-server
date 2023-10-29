9412fee651c688a6f79ab70d6c1948f32937165a
Rafael <rafael_guliev94@list.ru>
2023-10-29 14:35:52 +0300

    add socket io

--- app.js
+++ app.js
@@ -1,13 +1,43 @@
-const express = require('express');
+require("dotenv").config();
+const express = require("express");
 const app = express();
+const http = require("http");
 const port = process.env.PORT ?? 3000;
+const cors = require("cors");
+const index = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(index, {
+  cors: {
+    origin: "*",
+  },
+});
+app.use(cors());
 
-app.use(express.static('public'))
+app.use(express.static("public"));
 
-app.get('*', (req, res) => {
-    res.redirect('/');
-})
+const players = {};
 
-app.listen(port, () => {
-    console.log(`App listening on port ${port}`);
-})
+io.on("connection", (socket) => {
+  console.log("a user connected: ", socket.id);
+  players[socket.id] = {
+    x: Math.floor(Math.random() * 100) + 50,
+    y: Math.floor(Math.random() * 100) + 50,
+    id: socket.id,
+  };
+
+  socket.broadcast.emit("new_player", players[socket.id]);
+
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
+    // remove this player from our players object
+    // delete players[socket.id];
+  });
+});
+
+app.use(function (request, response) {
+  response.send("<h2>Hello</h2>");
+});
+
+index.listen(process.env.PORT || 8080, () => {
+  console.log(`listening on *:${process.env.PORT}`);
+});
